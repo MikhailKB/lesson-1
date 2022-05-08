b30be067bf0b872b79ed0c884281a11f0a5894c0
Михаил Козин <Vasago85@mail.ru>
2022-05-08 17:28:47 +0300

    Добавьте файлы проекта.

---
+++ Задание № 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using static System.Console;
+
+namespace lesson___1
+{
+    internal class Program
+    {
+        static void Main(string[] args)  // точка входа
+        {
+            WriteLine("Hello World!!!"); // вывод текста
+            
+            ReadKey();  // задержка
+        }
+    }
+}
